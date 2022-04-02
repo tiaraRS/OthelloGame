85d1eba4f21ccfe652e012ea649490f4be7a8268
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2022-04-02 11:32:09 -0300

    game play against computer

--- othelo_game_board.py
+++ othelo_game_board.py
@@ -1,4 +1,7 @@
+import pygame_textinput
 import pygame
+#import othelo_game
+from othelo_game import *
 
 pygame.init()
 def draw(state): 
@@ -30,17 +33,19 @@ def draw(state):
     y=w
 
     for row in state.game_board:
-        print("row",row)
+        #print("row",row)
         for col in row:
           #surf = pygame.Surface((w, w))
           #surf.fill((124,252,0))
-          print(col)
+          #print(col)
           pygame.draw.rect(screen, (124,252,0), pygame.Rect(x, y, w, w))
           pygame.draw.rect(screen, (205,92,92), pygame.Rect(x, y, w, w),  2)
           if col == 1:       
             pygame.draw.circle(screen, (255,255,255), (x+radius+5, y+radius+5), radius)     
           elif col == -1:
             pygame.draw.circle(screen, (0,0,0), (x+radius+5, y+radius+5), radius)   
+          elif col ==2:
+            pygame.draw.circle(screen, (0,0,0), (x+radius+5, y+radius+5), radius,2)   
             #surf.fill((0, 0, 0))  
 
               #pygame.draw.rect(screen, (205,92,92),surf, w, 30)  
@@ -81,12 +86,53 @@ def display_game_board(state):
     pos_values_player_MIN = np.where(game_board == 1)  
     pos_values_player_MIN = list(zip(pos_values_player_MIN[0],pos_values_player_MIN[1]))
 
-while 1:
-    game_board = State()
-    draw(game_board)
-    
+def get_action_from_click(pos):
+    positions = [range(70,140),range(140,210),range(210,280),range(280,350),range(350,420),range(420,490),range(490,560),range(560,630)] 
+    columns = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7}
+    map_values = {0:"A",1:"B",2:"C",3:"D",4:"E",5:"F",6:"G",7:"H"}
+    posx=-1
+    posy=-1
+    x = pos[0]
+    y = pos[1]
+    for i in range(len(positions)):
+        print(i)
+        if x in positions[i]:
+            posx = i
+        if y in positions[i]:
+            posy = i
+        if posx>-1 and posy>-1:
+            break
+    action = map_values[posx]+str(posy+1)
+    print(action)
+    return action
 
 
-
-      
-            
+game_state = State()
+possible_moves = actions(game_state)
+while len(possible_moves) > 0:         
+    draw(game_state)
+    pos_possible_actions_player = np.where(game_state.game_board == 2)
+    game_state.game_board[pos_possible_actions_player] = 0
+    #possible_moves, mat = actions(game_state)
+    if game_state.current_player == "MAX":
+        best_action = min_max_cutoff(game_state)
+        game_state = result(game_state, best_action.upper())
+    else:        
+        mouse_click = False
+        while not mouse_click:
+            events = pygame.event.get()  
+            for event in events:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_click = True
+                    pos = pygame.mouse.get_pos()            
+                    action = get_action_from_click(pos)
+                    game_state = result(game_state,action)
+    game_state.current_player = change_player(game_state)
+    possible_moves, mat = actions(game_state)    
+    pos_possible_actions_player = np.where(mat == 2)
+    #pos_possible_actions_player = list(zip(pos_possible_actions_player[0],pos_possible_actions_player[1]))
+    print("pos_possible_actions_player",pos_possible_actions_player)
+    game_state.game_board[pos_possible_actions_player] = 2
+    #pos_possible_actions_player = list(map(lambda action: str(map_values[action[1]])+str(action[0]+1), pos_possible_actions_player))
+    print("player",game_state.current_player)
+        
